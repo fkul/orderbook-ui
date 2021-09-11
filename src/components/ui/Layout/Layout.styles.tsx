7dd5e198d63db62a3fa58c8d5e182c079d47650a
Filip Kulpinski <filip.kulpinski@aeriagames.com>
2021-09-11 22:11:25 +0200

    Adjust global styles

--- src/components/ui/Layout/Layout.styles.tsx
+++ src/components/ui/Layout/Layout.styles.tsx
@@ -10,6 +10,9 @@ export const GlobalThemedStyles = styled.div`
     body {
       margin: 0;
       font-family: sans-serif;
+      font-size: 14px;
+      color: #afb2b9;
+      background-color: #212834;
     }
 
     *,
@@ -18,25 +21,20 @@ export const GlobalThemedStyles = styled.div`
       box-sizing: inherit;
     }
   }
-
-  color: var(--color-text-primary);
-  background: var(--color-bg-primary);
-  width: 100vw;
-  min-height: 100vh;
 `
 
 export const themeDefault = css`
   --color-primary: #4d37d0;
 
-  --color-text-primary: #adb0b7;
-  --color-text-secondary: #363b48;
+  --color-text-primary: #afb2b9;
+  --color-text-secondary: #434956;
 
   --color-bg-primary: #212834;
   --color-bg-secondary: #121723;
 
-  --color-ask-primary: #9b2b2f;
+  --color-ask-primary: #b83134;
   --color-ask-secondary: #351c24;
 
-  --color-bid-primary: #17895b;
+  --color-bid-primary: #198b5e;
   --color-bid-secondary: #142e2d;
 `
